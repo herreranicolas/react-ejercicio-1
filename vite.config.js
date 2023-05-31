2e17357cca8a79507b7cf969dffe5978baba9697
Nicolas Herrera <herreranicoo17@gmail.com>
2023-05-31 19:52:48 -0300

    Agregue la direccion del repo a la base de la config vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://herreranicolas.github.com/react-ejercicio-1",
+  base: "https://github.com/herreranicolas/react-ejercicio-1",
 });
